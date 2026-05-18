ecbd4ec4010ced17b0a914b1cfa7eea8129822ce
Sudip Koirala <soodeep321@gmail.com>
2026-05-18 19:11:07 +0545

    update

---
+++ models/user.ts
@@ -0,0 +1,72 @@
+import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+
+interface IUser extends Document {
+  userName: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  confirmPassword?: string;
+  _confirmPassword?: string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const UserSchema = new Schema<IUser>({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+// Virtual - fix the get() method
+UserSchema.virtual("confirmPassword")
+  .set(function (this: IUser, value: string) {
+    (this as any)._confirmPassword = value.trim();
+  })
+  .get(function (this: IUser) {
+    return (this as any)._confirmPassword;
+  });
+
+// Pre-validate hook - remove explicit next type
+UserSchema.pre<IUser>("validate", function (next: any) {
+  if (this.confirmPassword && this.confirmPassword !== this.password) {
+    return next(new Error("Passwords do not match"));
+  }
+  next();
+});
+
+// Pre-save hook - add proper typing
+UserSchema.pre<IUser>("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Instance method - add proper typing
+UserSchema.methods.comparePassword = async function (
+  this: IUser,
+  candidatePassword: string
+) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export const User =
+  mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
